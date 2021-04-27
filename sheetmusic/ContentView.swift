e02ab5f0e6c752b205b7679671d47b6fec5eeaf4
Lukas Werner <lwerner@lshigh.org>
2021-04-26 21:38:07 -0700

    Initial Commit

---
+++ sheetmusic/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  sheetmusic
+//
+//  Created by lukas on 4/26/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ sheetmusic/sheetmusicApp.swift
@@ -0,0 +1,17 @@
+//
+//  sheetmusicApp.swift
+//  sheetmusic
+//
+//  Created by lukas on 4/26/21.
+//
+
+import SwiftUI
+
+@main
+struct sheetmusicApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
